1ebf16a4656f665b206d8bd74a74642d695c262e
Justin Oliver <justindoliver@gmail.com>
2016-10-16 11:07:10 -0400

    Add a few tutorials

---
+++ boost_tutorial/lambda/bin/CMakeFiles/Lambda_Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lambda_Tutorial.dir/main.cpp.o"
+  "Lambda_Tutorial.pdb"
+  "Lambda_Tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lambda_Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
