7f2d171dea2ceb9a764149050058dadc65e59e5e
Justin Oliver <justindoliver@gmail.com>
2016-10-16 11:12:27 -0400

    updat

--- boost_tutorial/multi-index/main.cpp
+++ boost_tutorial/multi-index/main.cpp
@@ -1,13 +1,9 @@
-#include <boost/lambda/lambda.hpp>
+#include <boost/multi_index_container.hpp>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
 
 int main()
 {
-    using namespace boost::lambda;
-    typedef std::istream_iterator<int> in;
-
-    std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+    using namespace boost::lambda;    
 }
