d00a03a9b06481637e371c02a118b1668f4e6fd2
Justin Oliver <justindoliver@gmail.com>
2016-10-30 09:03:01 -0400

    edit main

--- boost_tutorial/multi-index/main.cpp
+++ boost_tutorial/multi-index/main.cpp
@@ -1,9 +1,23 @@
-#include <boost/multi_index_container.hpp>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <container.h>
+#include <employee.h>
+
+void print_out_by_name(const employee_set& es)
+{
+  // get a view to index #1 (name)
+  const employee_set::nth_index<1>::type& name_index = es.get<1>();
+  // use name_index as a regular std::set
+  std::copy(
+    name_index.begin(),name_index.end(),
+    std::ostream_iterator<employee>(std::cout));
+}
 
 int main()
 {
-    using namespace boost::lambda;    
+    using namespace boost;    
+    using namespace boost::multi_index;
+
+    return 0;  
 }
