342db7af97e8f05e194eaddc7119a748be2f0200
Justin Oliver <justindoliver@gmail.com>
2017-02-25 18:56:36 -0500

    Reformat and fix warnings

--- boost_tutorial/multi-index/test/test.cpp
+++ boost_tutorial/multi-index/test/test.cpp
@@ -7,76 +7,71 @@
 
 using ::testing::Return;
 
-class ContainerTest : public testing::Test {
-public:
-	ContainerTest() : d_ec( &d_mockDependency ), d_e1(1, "Zach"), d_e2(2,"Jed"), d_e3(3, "Alex") {}
-protected:
-	virtual void SetUp() {
-		
-		d_ec.add(d_e1);
-		d_ec.add(d_e2);
-		d_ec.add(d_e3);
-	}
+class ContainerTest : public testing::Test
+{
+  public:
+    ContainerTest() : d_ec(&d_mockDependency), d_e1(1, "Zach"), d_e2(2, "Jed"), d_e3(3, "Alex") {}
+  protected:
+    virtual void SetUp()
+    {
+        EXPECT_CALL(d_mockDependency, doTheThing(::testing::_)).Times(::testing::Exactly(3));
+        d_ec.add(d_e1);
+        d_ec.add(d_e2);
+        d_ec.add(d_e3);
+    }
 
-	virtual void TearDown() {
-		d_ec.clear();
-	}
-
+    virtual void TearDown()
+    {
+        d_ec.clear();
+    }
 
     MockDependency d_mockDependency;
-	EmployeeContainer d_ec;
-	Employee d_e1;
-	Employee d_e2;
-	Employee d_e3;
-
+    EmployeeContainer d_ec;
+    Employee d_e1;
+    Employee d_e2;
+    Employee d_e3;
 };
 
-TEST_F(ContainerTest, printByName) {
-  
-	d_ec.printByname();
-  	EXPECT_EQ(1, 1);
-}
-
-
-TEST_F( ContainerTest, add )
+TEST_F(ContainerTest, add)
 {
-    EXPECT_CALL( d_mockDependency, doTheThing(0) ).Times(::testing::Exactly(1));
+    EXPECT_CALL(d_mockDependency, doTheThing(::testing::_)).Times(::testing::Exactly(1));
 
-    Employee e0 (0, "Beth");
-    EXPECT_TRUE( d_ec.add( e0 ) );
+    Employee e0(0, "Beth");
+    EXPECT_TRUE(d_ec.add(e0));
 }
 
-TEST_F( ContainerTest, addDuplicate )
+TEST_F(ContainerTest, addDuplicate)
 {
-    EXPECT_CALL( d_mockDependency, doTheThing(0) ).Times(::testing::Exactly(1) );
-    
-    EXPECT_FALSE( d_ec.add( d_e1 ) );
+    EXPECT_CALL(d_mockDependency, doTheThing(::testing::_)).Times(::testing::Exactly(1));
+
+    EXPECT_FALSE(d_ec.add(d_e1));
 }
 
-TEST_F(ContainerTest, printById) {
-  
-  d_ec.printById();
+TEST_F(ContainerTest, printById)
+{
+
+    d_ec.printById();
     EXPECT_EQ(1, 1);
 }
 
+TEST_F(ContainerTest, printByname)
+{
 
-TEST_F(ContainerTest, printByname) {
-  
-  EXPECT_CALL( d_mockDependency, doTheOtherThing( ::testing::_, ::testing::_ ) ).Times(::testing::AtLeast(2) );
-  d_ec.printByname();
+    EXPECT_CALL(d_mockDependency, doTheOtherThing(::testing::_, ::testing::_)).Times(::testing::AtLeast(2));
+    d_ec.printByname();
     EXPECT_EQ(1, 1);
 }
 
-TEST_F(ContainerTest, findName ) {
-  
-  EXPECT_CALL( d_mockDependency, doTheSuperThing( ::testing::StrEq("theArgument")))
-  .Times(::testing::Exactly(2))
-  .WillOnce(Return(1));
+TEST_F(ContainerTest, findName)
+{
 
-  Employee e;
-  EXPECT_TRUE( d_ec.findByName( "Zach", e ) );
-  EXPECT_EQ( "Zach", e.name );
-  EXPECT_FALSE( d_ec.findByName( "Bob", e ) );
+    EXPECT_CALL(d_mockDependency, doTheSuperThing(::testing::StrEq("theArgument")))
+        .Times(::testing::Exactly(2))
+        .WillOnce(Return(100))
+        .WillOnce(Return(200));
+
+    Employee e;
+    EXPECT_TRUE(d_ec.findByName("Zach", e));
+    EXPECT_EQ("Zach", e.name);
+    EXPECT_FALSE(d_ec.findByName("Bob", e));
 }
-
-
